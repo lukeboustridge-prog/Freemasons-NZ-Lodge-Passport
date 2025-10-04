32f5acdd831fd548dbcf637695482e1d8cd74024
lukeboustridge-prog <luke.boustridge@gmail.com>
2025-10-04 14:29:06 +1300

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,2 +1,7 @@
 import type { Config } from "tailwindcss";
-export default { content: ["./index.html", "./src/**/*.{ts,tsx}"], theme: { extend: { container: { center: true, padding: "1rem" } } }, plugins: [], } satisfies Config;
+
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: { extend: {} },
+  plugins: [],
+} satisfies Config;
