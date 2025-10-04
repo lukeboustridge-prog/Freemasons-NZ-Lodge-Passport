3b60b5f87fbb40b5de251ede38f570820273c407
lukeboustridge-prog <luke.boustridge@gmail.com>
2025-10-04 13:03:05 +1300

    Delete index.ts

--- index.ts
+++
@@ -1,8 +0,0 @@
-export { default as FMNZLogo } from "./components/Logo";
-export { SectionCard } from "./components/SectionCard";
-export { default as Dashboard } from "./screens/Dashboard";
-export { MilestonesScreen } from "./screens/Milestones";
-export { VisitsScreen } from "./screens/Visits";
-export type { LodgeMembership, Office } from "./screens/Dashboard";
-export type { Milestone } from "./screens/Milestones";
-export type { Visit } from "./components/VisitFormModal";
